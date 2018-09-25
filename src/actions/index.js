b61e950d293ef10e3cf28fc526f301df3ec6b838
keith <kdang@quartermaster.house>
2018-09-25 18:53:07 -0400

    reversed http

--- src/actions/index.js
+++ src/actions/index.js
@@ -35,7 +35,7 @@ export function fetchWeather(current, forecast) {
 }
 export function fetchLocation(latitude, longitude) {
   const res = axios.get(
-    "http://nominatim.openstreetmap.org/reverse?format=json&lat=" +
+    "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
       latitude +
       "&lon=" +
       longitude +
