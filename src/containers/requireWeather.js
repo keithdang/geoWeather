0fe2e73c040cd1f55969e77a4bb594999590aa10
keith <kdang@quartermaster.house>
2018-09-25 18:10:02 -0400

    weather

---
+++ src/containers/requireWeather.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    render() {
+      return <ChildComponent {...this.props} />;
+    }
+  }
+  function mapStateToProps(state) {
+    return {
+      current: state.current,
+      week: state.week,
+      weather: state.weather
+    };
+  }
+  return connect(mapStateToProps)(ComposedComponent);
+};
